c737938e20e458d79c06627debaa9645aaf0e430
sep-marveltest <96120556+sep-marveltest@users.noreply.github.com>
2022-07-19 16:52:35 +0200

    Added components needed for v2 made by marc. No responsiveness or styling yet.

--- app_dummy.py
+++ app_dummy.py
@@ -1,6 +1,7 @@
 import plots
 import dash
 import json
+import dash_daq as daq
 import dash_table as dt
 import pandas as pd
 
@@ -38,58 +39,135 @@ server = app.server
 #                           APP (BEGIN)
 ###############################################################################
 
-app.layout = html.Div(children=[
-    html.Div(html.Img(src='assets/marvel_logo.png',
-                      style={'height':'20%', 'width':'20%'})),
+app.layout = html.Div(
+    id="root",
+    children=[
+        html.Div(
+            id="header",
+            children=[
+                html.Div(
+                    id="description-container",
+                    children=[
+                        html.H1(
+                            id="title",
+                            children="MarvelMaps"),
+                        html.P(
+                            id="description",
+                            children="A short introduction about the project and why it is of value to the user. \
+                            Disclaimer about privacy and safety of data being used. \
+                            First step, upload your data ->"
+                        )
+                    ]
+                ),
+                html.Div(
+                    id="logo-container",
+                    children=[
+                        html.H1(
+                            children="Marveltest Logo"
+                        )
+                    ]
+                )
+            ]
+        ),
+        html.Div(
+            id="app-container",
+            children=[
+                html.Div(
+                    id="options",
+                    children=[
+                        html.Div(
+                            id="metric-buttons-container",
+                            children=[
+                                html.Button('Total orders', id='button-total-orders', n_clicks=0),
+                                html.Button('Total clients', id='button-total-clients',  n_clicks=0),
+                                html.Button('Revenue', id='button-revenue', n_clicks=0),
+                                html.Button('Life Time Value', id='button-ltv', n_clicks=0),
+                                html.Button('Average Order Value', id='button-aov', n_clicks=0),
+                                html.Button('Clients per 100k pop', id='button-clients-100k', n_clicks=0)
+                            ]
+                        ),
+                        daq.BooleanSwitch(id='region-switch', on=False),
+                        html.Div(
+                            children=[html.Button('Update Figures', id='button_U')]
+                        ),
+                    ]
+                ),
 
-    dcc.RadioItems(
-        id='region_selection',
-        options=[
-            {'label': 'Provinces', 'value': 'Provinces'},
-            {'label': 'Counties', 'value': 'Counties'}
-        ],
-        value='Provinces'
-    ),
+                html.Div(
+                    id="marvelmap",
+                    children=[
+                        dcc.Graph(id='choropleth'),
+                    ]
+                ),
+                html.Div(
+                    id="marvelchart",
+                    children=[
+                        dcc.Graph(id='histogram')
+                    ]
+                ),
+                dt.DataTable(id='datatable', sort_action="native"),
+                dcc.Store(id="datasets", storage_type='session')
+            ]
+        )
+    ]
+)
 
-    html.Div([
-        dcc.Dropdown(
-        id='metric_selection',
-        options=options_dropdown,
-        value='clients'
-    )
-    ]),
 
-    html.Button('Update Figures', id='button_U'),
 
-    html.H3('* Dummy Dataset *'),
+# app.layout = html.Div(children=[
+#     html.Div(html.Img(src='assets/marvel_logo.png',
+#                       style={'height':'20%', 'width':'20%'})),
 
-    html.Div(children=[
-        dcc.Graph(id='choropleth',
-                style={'width': '50%', 'display': 'inline-block'}),
-        dcc.Graph(id='histogram',
-                  style={'width': '50%', 'display': 'inline-block'})],
-        style={'backgroundColor': '#13265290'}
-    ),
-    dt.DataTable(id='data-table',
-                 style_data = {
-                    'color': 'black',
-                    'backgroundColor': '#EFF4FA'
-    },
-                 style_header = {
-                    'backgroundColor': '#13265290',
-                    'fontWeight': 'bold'
-    },
-                 sort_action="native"
-    ),
+#     dcc.RadioItems(
+#         id='region-selection',
+#         options=[
+#             {'label': 'Provinces', 'value': 'Provinces'},
+#             {'label': 'Counties', 'value': 'Counties'}
+#         ],
+#         value='Provinces'
+#     ),
 
-    dcc.Store(id="datasets", storage_type='session')
+#     html.Div([
+#         dcc.Dropdown(
+#         id='metric_selection',
+#         options=options_dropdown,
+#         value='clients'
+#     )
+#     ]),
 
-], style={'margin': 'auto', 'width': '80%'})
+#     html.Button('Update Figures', id='button_U'),
+
+#     html.H3('* Dummy Dataset *'),
+
+#     html.Div(children=[
+#         dcc.Graph(id='choropleth',
+#                 style={'width': '50%', 'display': 'inline-block'}),
+#         dcc.Graph(id='histogram',
+#                   style={'width': '50%', 'display': 'inline-block'})],
+#         style={'backgroundColor': '#13265290'}
+#     ),
+#     dt.DataTable(id='data-table',
+#                  style_data = {
+#                     'color': 'black',
+#                     'backgroundColor': '#EFF4FA'
+#     },
+#                  style_header = {
+#                     'backgroundColor': '#13265290',
+#                     'fontWeight': 'bold'
+#     },
+#                  sort_action="native"
+#     ),
+
+#     dcc.Store(id="datasets", storage_type='session')
+
+# ], style={'margin': 'auto', 'width': '80%'})
 
 
 ###############################################################################
 #                           APP (END)
 ###############################################################################
+
+
 
 @app.callback(Output('datasets', 'data'),
               Input('button_U', 'n_clicks'),
@@ -108,7 +186,7 @@ def load_data(n_clicks, datasets_json):
               Input('button_U', 'n_clicks'),
               State('datasets', 'data'),
               State('metric_selection', 'value'),
-              State('region_selection', 'value'))
+              State('region-selection', 'value'))
 def update_figures(n_clicks, datasets_json, metric, region):
     changed_id = [p['prop_id'] for p in callback_context.triggered][0]
 
@@ -132,7 +210,7 @@ def update_figures(n_clicks, datasets_json, metric, region):
 
 
 @app.callback(Output("metric_selection", "options"),
-              Input("region_selection", "value"))
+              Input("region-selection", "value"))
 def update_dropdown(region):
     if region == 'Provinces':
         return options_dropdown + [
